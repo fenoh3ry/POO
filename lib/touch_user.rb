3632ddfe1b1ba94e19e8edf32c11dae3a6a8c402
fenohery.fanomezanirina@gmail.com <fenohery.fanomezanirina@gmail.com>
2019-07-23 11:21:58 +0300

    corrected POO

---
+++ lib/touch_user.rb
@@ -0,0 +1,9 @@
+require "pry"
+class User
+	def greet
+		puts "Bonjour, Monde !"
+	end
+end #fin de ma classe
+
+binding.pry
+puts "end of file"
---
+++ lib/usertest.rb
@@ -0,0 +1,16 @@
+require "pry"
+
+class User
+
+end
+
+
+
+mon_instance = User.new
+
+anto = User.new
+
+fina = User.new
+
+puts anto
+puts fina
